6d33b648bd0df87c255142f7d1d40c7396cb50ca
Zakari Suleiman <zaksman2019@gmail.com>
2025-06-05 11:22:42 +0100

    Refactor time update function to display GMT+1 instead of UTC

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 let time = document.getElementById("time");
 
-function updateTime() {
-  let fullTime = new Date().toUTCString();
-  time.innerHTML = `${fullTime}`;
+function updateTimeGMTPlus1() {
+  let now = new Date();
+  // Get UTC time in milliseconds, add 1 hour (3600000 ms)
+  let gmtPlus1 = new Date(now.getTime() + 60 * 60 * 1000);
+  // Format as string
+  let options = {
+    timeZone: "Etc/GMT-1",
+    hour12: false,
+    weekday: "short",
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  };
+  time.innerHTML = gmtPlus1.toLocaleString("en-GB", options) + " GMT+1";
 }
-updateTime();
-setInterval(updateTime, 1000);
+// Update the time every second
+setInterval(updateTimeGMTPlus1, 1000);
+updateTimeGMTPlus1();
 
 const check = document.getElementById("checkbox");
 let container = document.getElementById("contain");
